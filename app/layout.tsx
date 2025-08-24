00ef506d66d95d0916643197431b63fcaa1d76fd
Fedjosity <fedjostayomide@gmail.com>
2025-08-24 02:08:54 +0100

    refactor: remove unnecessary attribute from ThemeProvider in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider defaultTheme="dark" attribute="class">
+        <ThemeProvider defaultTheme="dark">
           <Navigation />
           <main className="min-h-screen">{children}</main>
           <Footer />
